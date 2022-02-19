16cb102d71f94462a5d294108f15bf7b91a885a3
reemtarek <reemtarekmatr97@gmail.com>
2022-02-19 07:50:56 +0200

    first

---
+++ Services/IProductsService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorServerDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorServerDemo.Services
+{
+    public interface IProductsService
+    {
+        Task<List<Products>> GetProducts();
+        Task<Products> CreateProduct(Products product);
+        Task<bool> UpdateProduct(Products product);
+        Task<bool> DeleteProduct(Products product);
+    }
+}
---
+++ Services/ProductsService.cs
@@ -0,0 +1,60 @@
+﻿using BlazorServerDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerDemo.Services
+{
+    public class ProductsService : IProductsService
+    {
+        private readonly BlazortestContext _context;
+
+        public ProductsService(BlazortestContext context)
+        {
+            _context = context;
+        }
+
+        public async  Task<List<Products>> GetProducts()
+        {
+           
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Products> CreateProduct(Products product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return await Task.FromResult(product);
+        }
+
+        public Task<bool> UpdateProduct(Products product)
+        {
+            var productCheck = _context.Products.Where(x => x.Id == product.Id).FirstOrDefault();
+            if (productCheck != null)
+            {
+                productCheck.ProductName = product.ProductName;
+                _context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> DeleteProduct(Products product)
+        {
+            var productCheck = _context.Products.Where(x => x.Id == product.Id).FirstOrDefault();
+            if (productCheck != null)
+            {
+                _context.Products.Remove(productCheck);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+    }
+}
